3bdb64d1cbb6cfded1f99e8fbab77e949842f969
root <root@vs-michaelzwiener.au.syrahost.com>
2013-03-05 12:54:48 +0800

    Working version

---
+++ gitcreatetag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Checkout the branch
+cd /home/rcn2/committest
+git checkout prod
+git pull
+git checkout $1
+
+# Create tag
+git tag -a $1 -m "Tag for $1"
+
+# Delete branch
+git checkout prod
+git push origin :$1
+git branch -D $1
+
+# Push tag
+git push origin $1
+
+
+echo "Done with git tag"
